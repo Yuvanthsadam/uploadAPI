51b872e7daa39fbd2a9806b3a8f6011b5ae094e2
Yuvanth <sadamyuvanth@gmail.com>
2022-05-26 17:07:51 +0530

    changes in  urls

--- uploadAPI/urls.py
+++ uploadAPI/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from uploadAPI.views import proListView,proDetailView
 
 urlpatterns = [
-path('profile/', proListView.as_view(), name='pro-list'),
+path('profile', proListView.as_view(), name='pro-list'),
 path('profile/<int:pk>', proDetailView.as_view(), name='pro-detail'),
 ]
